ecd0f369e2a262d35c95e8e89ea8a3573a9bb5d8
yuhaoqiang <qhyu_144s@126.com>
2023-08-11 12:36:03 +0800

    good chat

--- roundchat_app.py
+++ roundchat_app.py
@@ -8,6 +8,10 @@ from langchain.schema import(
     HumanMessage,
     AIMessage
 )
+from langchain.llms import OpenAI
+
+
+st.set_page_config(page_title="Tyxus")
 
 def init():
     load_dotenv()
@@ -21,39 +25,46 @@ def main():
     
     # init()
     
-    with st.sidebar:
-        user_input1 = st.text_input("your api_key: ",key="user_input1")
+    api_key_t = st.sidebar.text_input("your api: ",key="user_input1")
     
-    chat = ChatOpenAI(temperature=0, openai_api_key=user_input1)
-    if "messages" not in st.session_state:
-        st.session_state.messages = [
-            SystemMessage(content="you are a helpful assistant.")
-        ]
     
-    st.set_page_config(
-        page_title="Tyxus",
-        # page_icon="00"
-    )
-
-    st.header("自由的GPT")
-
-    with st.sidebar:
-        user_input = st.text_input("your message: ",key="user_input")
+    if "api_key" not in st.session_state:
+        st.session_state.api_key = ""
+    if api_key_t != "":
+        st.session_state.api_key = api_key_t
         
-        if user_input:
-            # message(user_input, is_user=True)
-            st.session_state.messages.append(HumanMessage(content=user_input))
-            with st.spinner("Thinking..."):
-                response = chat(st.session_state.messages) 
-            st.session_state.messages.append(AIMessage(content=response.content))
-            # message(response.content, is_user=False)
-    
-    messages = st.session_state.get('messages',[])
-    for i, msg in enumerate(messages[1:]):
-        if i % 2 ==0:
-            message(msg.content, is_user=True, key=str(i)+"_user")
-        else:
-            message(msg.content, is_user=False, key=str(i)+"_ai")
+    if st.session_state.api_key:
+        chat = ChatOpenAI(temperature=0, openai_api_key=st.session_state.api_key)
+
+        if "messages" not in st.session_state:
+            st.session_state.messages = [
+                SystemMessage(content="you are a helpful assistant.")
+            ]
+
+        # st.set_page_config(
+        #     page_title="Tyxus"
+        #     # page_icon="00"
+        # )
+
+        st.header("自由的GPT")
+
+        with st.sidebar:
+            user_input = st.text_input("your message: ",key="user_input")
+            
+            if user_input:
+                # message(user_input, is_user=True)
+                st.session_state.messages.append(HumanMessage(content=user_input))
+                with st.spinner("Thinking..."):
+                    response = chat(st.session_state.messages) 
+                st.session_state.messages.append(AIMessage(content=response.content))
+                # message(response.content, is_user=False)
+        
+        messages = st.session_state.get('messages',[])
+        for i, msg in enumerate(messages[1:]):
+            if i % 2 ==0:
+                message(msg.content, is_user=True, key=str(i)+"_user")
+            else:
+                message(msg.content, is_user=False, key=str(i)+"_ai")
 
 if __name__ == "__main__":
     main()
